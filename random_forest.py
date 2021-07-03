407010d9204b62ffccc5b9d5f5261602be78b15a
naman <naman.jain@btech2015.iitgn.ac.in>
2021-07-03 16:08:13 +0530

    wip

--- random_forest.py
+++ random_forest.py
@@ -3,13 +3,19 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import accuracy_score
+import pydot
+from sklearn.tree import export_graphviz
 
 
 if __name__ == "__main__":
 
     features_csv_path = "elon_features/elonmusk_features.csv"
+    out_dir = "random_forest"
+
+    os.makedirs(out_dir, exist_ok=True)
     
-    features_df = pd.read_csv(features_csv_path)
+    features_df = pd.read_csv(features_csv_path, index_col=0)
     # defining labels
     labels = np.array(features_df['tweet_bool'])
 
@@ -42,10 +48,35 @@ if __name__ == "__main__":
     # Print out the mean absolute error (mae)
     print('Mean Absolute Error:', round(np.mean(errors), 2))
 
-    # Calculate mean absolute percentage error (MAPE)
-    import ipdb
-    ipdb.set_trace()
-    mape = 100 * (errors / test_labels)
-    # Calculate and display accuracy
-    accuracy = 100 - np.mean(mape)
-    print('Accuracy:', round(accuracy, 2), '%.')
+    # Get accuracy
+    predicted_labels = np.round(predictions)
+    accuracy = accuracy_score(test_labels, predicted_labels)   
+    print('Accuracy:', accuracy*100, "%")
+
+    # saving gt and predicted labels
+    data = {"gt_labels": test_labels.tolist(), "predicted_labels": predicted_labels.tolist()}
+    df = pd.DataFrame(data)
+    df.to_csv(os.path.join(out_dir, "gt_and_predicted_labels.csv"))
+
+
+    # exporting visualization for a single decision tree
+    # Pull out one tree from the forest
+    tree = rf.estimators_[5]
+    # Export the image to a dot file    
+    feature_list = list(features_df.columns)
+    export_graphviz(tree, out_file = os.path.join(out_dir, 'tree.dot'), feature_names = feature_list, rounded = True, precision = 1)
+    # Use dot file to create a graph
+    (graph, ) = pydot.graph_from_dot_file(os.path.join(out_dir, 'tree.dot'))
+    # Write graph to a png file
+    graph.write_png(os.path.join(out_dir, 'tree.png'))
+
+    # Get numerical feature importances
+    importances = list(rf.feature_importances_)
+    # List of tuples with variable and importance
+    feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
+    # Sort the feature importances by most important first
+    feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+    # Print out the feature and importances 
+    [print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances]
+
+
